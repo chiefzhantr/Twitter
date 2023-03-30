3573624e3e3ee955be8c9cf9d2e65aa9fd043de7
Temirkhan <temirkhan.seken@mail.ru>
2023-03-30 11:46:54 +0600

    component generation

--- twitter-front/src/app/app.module.ts
+++ twitter-front/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { NewsComponent } from './news/news.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SettingsComponent } from './settings/settings.component';
+import { LoginSignUpComponent } from './login-sign-up/login-sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    SearchbarComponent,
+    NewsComponent,
+    ProfileComponent,
+    SettingsComponent,
+    LoginSignUpComponent
   ],
   imports: [
     BrowserModule,
