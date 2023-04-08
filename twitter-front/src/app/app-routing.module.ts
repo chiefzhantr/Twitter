9e4f79d3428b464c04daf6a0a42cd3fa273feb83
iitsdim <dimash241@gmail.com>
2023-04-08 13:03:12 +0600

    adding post detail route

--- twitter-front/src/app/app-routing.module.ts
+++ twitter-front/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {NewsComponent} from "./news/news.component";
 import {SettingsComponent} from "./settings/settings.component";
+import {PostDetailComponent} from "./post-detail/post-detail.component";
 
 const routes: Routes = [
   {path: 'news', component: NewsComponent},
   {path: 'settings', component: SettingsComponent},
+  {path: 'news/:postId', component: PostDetailComponent},
 ];
 
 @NgModule({
