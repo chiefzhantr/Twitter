1c7f6ee93ed045454c73be4c9e3b5b7efb3e4581
Nauan <@n.akylbaev>
2023-04-11 00:04:48 +0600

    Added html markup& css styling of Login-Signup

--- twitter-front/src/app/app-routing.module.ts
+++ twitter-front/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginSignUpComponent} from "./login-sign-up/login-sign-up.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginSignUpComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
