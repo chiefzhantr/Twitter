0a3d69e45cfa84b2197bfca069778ba5d3e9290b
Zhantore <@n.akylbaev>
2023-04-19 19:15:59 +0600

    changed routing system and added edit page for post

--- twitter-front/src/app/sidebar/sidebar.component.ts
+++ twitter-front/src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-sidebar',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class SidebarComponent {
 
+  constructor(private router: Router) {
+  }
+  showNews() {
+    this.router.navigate(['news'])
+  }
+  showProfile() {
+    this.router.navigate(['profile'])
+  }
+  showSettings() {
+    this.router.navigate(['settings'])
+  }
 }
