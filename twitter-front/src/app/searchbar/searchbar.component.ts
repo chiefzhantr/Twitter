46357817f69c797882928a23c218af506d0da237
iitsdim <dimash241@gmail.com>
2023-04-20 12:44:13 +0600

    added routing to searchbar

--- twitter-front/src/app/searchbar/searchbar.component.ts
+++ twitter-front/src/app/searchbar/searchbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-searchbar',
@@ -9,14 +10,20 @@ export class SearchbarComponent {
   selectedOption: string = 'Post';
   searchText: string = '';
 
+  constructor(
+    private router: Router
+  ) {
+  }
+
   onSearch(): void {
     switch(this.selectedOption) {
       case 'Post':
         console.log(`Searching for "${this.searchText}" in Post`);
-
+        this.router.navigate(['news'])
         break;
       case 'Person':
         console.log(`Searching for "${this.searchText}" in Person`);
+        this.router.navigate(['profile'])
         break;
       default:
         console.log(`Unknown option: ${this.selectedOption}`);
