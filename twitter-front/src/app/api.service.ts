1fb7af92ea3b33f13b8826170c0d045f367ce219
Zhantore <@n.akylbaev>
2023-04-20 11:48:22 +0600

    changed routing system and added edit page for post

---
+++ twitter-front/src/app/api.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of} from 'rxjs';
+import { User } from 'src/app/models/user';
+import { Post } from 'src/app/models/post';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private baseUrl = 'https://twitter.kz/api';
+
+  mockUsers : User[];
+  mockPosts : Post[];
+
+  constructor(private http: HttpClient) {
+    this.mockPosts = [
+      {
+        "id": 1,
+        "username": "Dimmyt",
+        "user_id": 1,
+        "profilePicture": "1.jpeg",
+        "body": "MCI:BAY 3:0"
+      } as Post
+    ];
+    this.mockUsers = [
+      {
+        "username": "Dimmyt",
+        "first_name": "Ali",
+        "last_name": "Soldatbay",
+        "phone_number": "+77077441212",
+      } as User
+    ];
+
+  }
+
+  getUser(username: string): Observable<User> {
+    //uncomment when api will be ready
+    // const url = `${this.baseUrl}/users/${username}`;
+    // return this.http.get<User>(url);
+    const user = this.mockUsers[0];
+    return of(user);
+  }
+
+  getPosts(username: string): Observable<Post[]> {
+    //uncomment when api will be ready
+    // const url = `${this.baseUrl}/users/${username}/posts`;
+    // return this.http.get<Post[]>(url);
+    return of(this.mockPosts);
+  }
+
+}
---
+++ twitter-front/src/app/edit-post/edit-post.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import {ApiService} from "../api.service";
+
+@Component({
+  selector: 'app-edit-post',
+  templateUrl: './edit-post.component.html',
+  styleUrls: ['./edit-post.component.css']
+})
+export class EditPostComponent {
+
+  constructor(
+    // private apiService: ApiService
+  ) {
+  }
+
+  deletePost() {
+  //   this.apiService.deletePost(post.id).subs
+  }
+
+  updatePost() {
+//   this.apiService.updatePost(post.id).
+  }
+}
