fbd3374b9b28e043fd6a127b8725e5d6b2a53fce
iitsdim <dimash241@gmail.com>
2023-04-25 17:16:47 +0600

    Added posts and media models and dependency between TODO: add views and serializers

--- twitter_back/api/admin.py
+++ twitter_back/api/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 
-from api.models import User
+from api.models import User, Post
 
 
 # Register your models here.
 @admin.register(User)
 class CompanyAdmin(admin.ModelAdmin):
     list_display = ('id', 'username', 'first_name', 'last_name', 'profile_picture', 'password', 'phone_number')
+
+
+admin.site.register(Post)
--- twitter_back/api/models.py
+++ twitter_back/api/models.py
@@ -18,19 +18,7 @@ class User(models.Model):
         return f'{self.id}: {self.username}'
 
 
-class Media(models.Model):
-    url = models.TextField(max_length=512)
-
-    class Meta:
-        verbose_name = 'Media'
-        verbose_name_plural = 'Medias'
-
-    def __str__(self):
-        return f'{self.id}: {self.url}'
-
-
 class Post(models.Model):
-    username = models.CharField(max_length=255, null=False)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user', null=False)
     body = models.TextField(max_length=512, null=False)
 
@@ -39,7 +27,22 @@ class Post(models.Model):
         verbose_name_plural = 'Posts'
 
     def __str__(self):
-        return f'{self.id}: {self.username}'
+        return f'{self.id}: {self.user_id.username}'
 
     def get_profile_picture(self):
         return self.user_id.profile_picture
+
+    def get_username(self):
+        return self.user_id.username
+
+
+class Media(models.Model):
+    url = models.TextField(max_length=512)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Media'
+        verbose_name_plural = 'Medias'
+
+    def __str__(self):
+        return f'{self.id}: {self.url}'
