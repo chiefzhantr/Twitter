aef8ef3ed09c6189ca7b82b2e40e6efc0fb0e334
Zhantore <@n.akylbaev>
2023-04-26 09:42:58 +0600

    Created a CRUD&Serializer to Tweet

--- twitter_back/api/serializers.py
+++ twitter_back/api/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
 
-from api.models import User
+from api.models import User, Tweet
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'first_name', 'last_name', 'profile_picture', 'password', 'phone_number')
+
+# class TweetSerializer(serializers.Serializer):
+#     username = serializers.CharField(max_length=255)
+#     user_id = serializers.IntegerField(read_only=True)
+#     profilePicture = serializers.CharField()
+#     body = serializers.CharField()
+#     post_id = serializers.IntegerField()
+
+class TweetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tweet
+        fields = ('__all__')
--- twitter_back/api/urls.py
+++ twitter_back/api/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     # path("", views.index, name="index"),
     path("login/", obtain_jwt_token),
     path("users/", views.UserListAPIView.as_view()),
-    path("users/<int:id>/", views.UserDetailAPIView.as_view())
+    path("users/<int:id>/", views.UserDetailAPIView.as_view()),
+    path('post/<int:pk>/tweets/', TweetListView.as_view()),
+#     path('post/<int:pk>/tweets/create/', TweetCreateView.as_view()),
+#     path('post/<int:pk>/tweets/<int:pk>/update/', TweetUpdateView.as_view()),
+#     path('post/<int:pk>/tweets/<int:pk>/delete/', TweetDeleteView.as_view()),
 ]
+
