d840a21e6e3c233921fe5a3566662161027c3653
iitsdim <dimash241@gmail.com>
2023-04-26 11:50:55 +0600

    Added serializers and views

--- twitter_back/api/serializers.py
+++ twitter_back/api/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
 
-from api.models import User
+from api.models import User, Post, Media
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'first_name', 'last_name', 'profile_picture', 'password', 'phone_number')
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
+class MediaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Media
+        fields = '__all__'
--- twitter_back/api/views.py
+++ twitter_back/api/views.py
@@ -1,6 +1,68 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import json
+
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from api.models import Post, Media
+from api.serializers import PostSerializer, MediaSerializer
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+@csrf_exempt
+def posts_list(request):
+    if request.method == 'GET':
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+
+        for post in serializer.data:
+            post["medias"] = Media.objects.filter(post_id=post)
+
+        return JsonResponse(serializer.data, safe=False)
+    if request.method == 'POST':
+        data = json.loads(request.body)
+
+        if hasattr(data, 'medias'):
+            medias = data.medias
+            del data.medias
+
+        serializer = PostSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        for media_url in medias:
+            media_serializer = MediaSerializer(
+                {
+                    "url": media_url,
+                    "post_id": serializer.data.id
+                }
+            )
+
+            media_serializer.is_valid(raise_exception=True)
+            serializer.save()
+
+        return JsonResponse(serializer.data)
+
+
+@csrf_exempt
+def post_retrieve(request, pk):
+    try:
+        post = get_object_or_404(Post, pk=pk)
+    except Post.DoesNotExist as e:
+        return JsonResponse({'error': str(e)}, status=404)
+
+    if request.method == 'GET':
+        serializer = PostSerializer(post)
+        return JsonResponse(serializer.data)
+    elif request.method == 'DELETE':
+        post.delete()
+        return JsonResponse({'deleted': True})
+    elif request.method == 'PUT':
+        data = json.loads(request.body)
+        serializer = PostSerializer(instance=post, data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return JsonResponse(serializer.data)
